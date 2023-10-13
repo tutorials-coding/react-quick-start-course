f3e2665dfc130ec31e46bf6ec32dee38caacb4ad
Vadim <korobeinikovv@mail.ru>
2023-10-13 23:08:14 +0300

    Добавлен и применен глобальный css класс my-text

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
       <div className="col-5">
         <img src="./next.svg" alt="logo" />
       </div>
-      <div className="col-7">Мой список чтения</div>
+      <div className="col-7 my-text">Мой список чтения</div>
     </header>
   );
 };
