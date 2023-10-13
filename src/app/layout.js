ba1980c98bba8b43e4385b9b641018f18ae788fc
Vadim <korobeinikovv@mail.ru>
2023-10-13 22:51:12 +0300

    Добавили bootstrap из cdn и применили css класс

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className={inter.className}>
+        <div className="container">{children}</div>
+      </body>
     </html>
   );
 }
