567526ba43939a9902d0395fde86b6d20a6b7175
Vadim <korobeinikovv@mail.ru>
2023-10-13 23:24:02 +0300

    Добавлены стили

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -2,17 +2,17 @@ import styles from './Title.module.css';
 
 const textStyles = {
   fontSize: "18px",
-  color: "green",
+  color: "gray",
   fontWeight: 700
 }
 
 const Title = () => {
   return (
-    <header className="row">
+    <header className="row p-4">
       <div className="col-5">
         <img src="./next.svg" alt="logo" />
       </div>
-      <div className="col-7">
+      <div className="col-7 text-center">
         <p className={styles.myText}>Мой список чтения</p>
         <p style={textStyles}>Книги по программированию</p>
       </div>
