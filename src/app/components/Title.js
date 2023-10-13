5e44e61b4c5ba382828710ed221defc209ebb65f
Vadim <korobeinikovv@mail.ru>
2023-10-13 23:12:06 +0300

    Добавлены css модули для Title

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -1,10 +1,14 @@
+import styles from './Title.module.css';
+
 const Title = () => {
   return (
     <header className="row">
       <div className="col-5">
         <img src="./next.svg" alt="logo" />
       </div>
-      <div className="col-7 my-text">Мой список чтения</div>
+      <div className="col-7">
+        <p className={styles.myText}>Мой список чтения</p>
+      </div>
     </header>
   );
 };
