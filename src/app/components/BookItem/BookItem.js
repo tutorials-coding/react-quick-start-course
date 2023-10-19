24ae03b5be0b4b6d6bf94c0295614ec687bceae8
Vadim <korobeinikovv@mail.ru>
2023-10-19 22:14:22 +0300

    Code cleanup

--- src/app/components/BookItem/BookItem.js
+++ src/app/components/BookItem/BookItem.js
@@ -6,11 +6,9 @@ const BookItem = ({ book }) => {
       <td className="p-2">{book.id}</td>
       <td className="p-2">{book.author}</td>
       <td className="p-2">{book.title}</td>
-      {book.isRead && (
-        <td className={`p-2 ${book.isRead ? 'text-success' : ''}`}>
-          {String(book.isRead)}
-        </td>
-      )}
+      <td className={`p-2 ${book.isRead ? 'text-success' : ''}`}>
+        {String(book.isRead)}
+      </td>
     </tr>
   );
 };
