090212381c063fd0f3f6c35358f6a3df9e156d32
Vadim <korobeinikovv@mail.ru>
2023-10-19 22:14:02 +0300

    Добавлено условие отображения BookCard и ReadingList

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,11 +1,17 @@
+'use client';
+
+import { useState } from 'react';
 import Title from './Title/Title';
 import ReadingList from './ReadingList/ReadingList';
+import BookCard from './BookCard/BookCard';
 
 const App = () => {
+  const [selectedBook, setSelectedBook] = useState();
+
   return (
     <>
       <Title>Мой список для чтения</Title>
-      <ReadingList />
+      {selectedBook ? <BookCard book={selectedBook} /> : <ReadingList />}
     </>
   );
 };
