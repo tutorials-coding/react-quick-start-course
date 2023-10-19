5a622826cced6e2a72aee1cd0c1ff0f55f75e07d
Vadim <korobeinikovv@mail.ru>
2023-10-19 19:58:22 +0300

    Добавлена проверка при показе ячейка Прочитано

--- src/app/components/BookItem/BookItem.js
+++ src/app/components/BookItem/BookItem.js
@@ -6,9 +6,11 @@ const BookItem = ({ book }) => {
       <td className="p-2">{book.id}</td>
       <td className="p-2">{book.author}</td>
       <td className="p-2">{book.title}</td>
-      <td className={`p-2 ${book.isRead ? 'text-success' : ''}`}>
-        {String(book.isRead)}
-      </td>
+      {book.isRead && (
+        <td className={`p-2 ${book.isRead ? 'text-success' : ''}`}>
+          {String(book.isRead)}
+        </td>
+      )}
     </tr>
   );
 };
