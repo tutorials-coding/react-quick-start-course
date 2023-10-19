2309744269320afdd1dc4597e6bac80ff01322e2
Vadim <korobeinikovv@mail.ru>
2023-10-19 19:56:43 +0300

    Добавлен условный рендеринг (вариант 2)

--- src/app/components/BookItem/BookItem.js
+++ src/app/components/BookItem/BookItem.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
 const BookItem = ({ book }) => {
-  let isReadTd;
-
-  if (book.isRead) {
-    isReadTd = <td className="p-2 text-success">{String(book.isRead)}</td>;
-  } else {
-    isReadTd = <td className="p-2">{String(book.isRead)}</td>;
-  }
-
   return (
     <tr>
       <td className="p-2">{book.id}</td>
       <td className="p-2">{book.author}</td>
       <td className="p-2">{book.title}</td>
-      {isReadTd}
+      <td className={`p-2 ${book.isRead ? 'text-success' : ''}`}>
+        {String(book.isRead)}
+      </td>
     </tr>
   );
 };
