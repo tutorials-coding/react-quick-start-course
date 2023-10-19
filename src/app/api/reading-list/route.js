c250530c560db87e4d66cf4222b56b67137669e8
Vadim <korobeinikovv@mail.ru>
2023-10-19 20:29:24 +0300

    Добавлено поле image в api

--- src/app/api/reading-list/route.js
+++ src/app/api/reading-list/route.js
@@ -5,18 +5,21 @@ const readingList = [
     id: 1,
     title: 'Чистый код: Создание, анализ и рефакторинг',
     author: 'Роберт С. Мартин',
+    image: 'img-1',
     isRead: true,
   },
   {
     id: 2,
     title: 'Карьера программиста',
     author: 'Г. Лакман Макдауэлл',
+    image: 'img-2',
     isRead: true,
   },
   {
     id: 3,
     title: 'Совершенный код',
     author: 'Стив Макконнелл',
+    image: 'img-3',
     isRead: false,
   },
   {
@@ -24,6 +27,7 @@ const readingList = [
     title:
       'Рефакторинг кода на JavaScript: улучшение проекта существующего кода',
     author: 'Мартин Фаулер',
+    image: 'img-4',
     isRead: false,
   },
 ];
