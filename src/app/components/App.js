af2af6292df82434cd10fdb029ba189d6b09c204
Vadim <korobeinikovv@mail.ru>
2023-10-21 11:54:30 +0300

    Передано состояние nav в контекст

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -9,8 +9,9 @@ import { navValues } from '../const/nav-values';
 export const navContext = createContext(navValues.book);
 
 const App = () => {
+  const [nav, setNav] = useState(navValues.book);
   return (
-    <navContext.Provider value={navValues.book}>
+    <navContext.Provider value={nav}>
       <Title>Мой список для чтения</Title>
     </navContext.Provider>
   );
