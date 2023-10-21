537d05f013d7af858079bb864943ebaa36b359ba
Vadim <korobeinikovv@mail.ru>
2023-10-21 13:11:38 +0300

    Обновлено лого

--- src/app/components/Title/Title.js
+++ src/app/components/Title/Title.js
@@ -3,8 +3,8 @@ import style from './Title.module.css';
 const Title = ({ children }) => {
   return (
     <header className="row p-4">
-      <div className="col-5">
-        <img src="./next.svg" alt="logo" />
+      <div className={`col-5 ${style.logoContainer}`}>
+        <img src="./book-images/default-book.jpeg" alt="logo" />
       </div>
       <div className="col-7 text-center">
         <h1 className={style.myText}>{children}</h1>
