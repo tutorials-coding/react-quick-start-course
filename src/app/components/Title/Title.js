988f6d52e407e0a40507faffa3837a54ed2abd0f
Vadim <korobeinikovv@mail.ru>
2023-10-22 12:33:51 +0300

    Добавлена навигация из лого

--- src/app/components/Title/Title.js
+++ src/app/components/Title/Title.js
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
 import style from './Title.module.css';
+import { navContext } from '../App';
+import { navValues } from '../../const/nav-values';
 
 const Title = ({ children }) => {
+  const { navigate } = useContext(navContext);
   return (
     <header className="row p-4">
-      <div className={`col-5 ${style.logoContainer}`}>
+      <div
+        className={`col-5 ${style.logoContainer}`}
+        onClick={() => navigate(navValues.books)}
+      >
         <img src="./book-images/default-book.jpeg" alt="logo" />
       </div>
       <div className="col-7 text-center">
